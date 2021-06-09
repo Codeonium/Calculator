5aaa17122d59df9235c49896ed0033b895d9e4dc
codeonium <codeon2020@gmail.com>
2021-06-09 13:50:50 +0100

    final commit

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -11,52 +11,64 @@ describe('App.vue', () => {
     expect(wrapper.vm.runningTotal).to.equal(9)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can add 1 to 4', () => {
     const wrapper = shallowMount(App)
     wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.add('1');
+    expect(wrapper.vm.runningTotal).to.equal(5)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can subtract 4 from 7', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.previousTotal = 7
+    wrapper.vm.subtract('4');
+    expect(wrapper.vm.runningTotal).to.equal(3)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can multiply 3 by 5', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.previousTotal = 3
+    wrapper.vm.multiply('5');
+    expect(wrapper.vm.runningTotal).to.equal(15)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can divide 21 by 7', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.previousTotal = 21
+    wrapper.vm.divide('7');
+    expect(wrapper.vm.runningTotal).to.equal(3)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can concatenate multiple number button clicks', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.numberClick('4');
+    wrapper.vm.numberClick('5');
+    expect(wrapper.vm.runningTotal).to.equal(45)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can chain multiple operations together', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.numberClick('4');
+    wrapper.vm.operatorClick('+')
+    wrapper.vm.numberClick('5');
+    wrapper.vm.operatorClick('*')
+    wrapper.vm.numberClick('5');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(45)
   })
 
-  xit('enterNum changes running total', () => {
+  it('Can clear the running total without affecting the calculation', () => {
     const wrapper = shallowMount(App)
-    wrapper.vm.previousTotal = 4
-    wrapper.vm.add('5');
-    expect(wrapper.vm.runningTotal).to.equal(9)
+    wrapper.vm.numberClick('4');
+    wrapper.vm.operatorClick('+')
+    wrapper.vm.numberClick('5');
+    wrapper.vm.operatorClick('+')
+    wrapper.vm.numberClick('5');
+    wrapper.vm.clearClick()
+    expect(wrapper.vm.runningTotal).to.equal(0)
+    wrapper.vm.operatorClick('+')
+    wrapper.vm.numberClick('9');
+    wrapper.vm.operatorClick('=');
+    expect(wrapper.vm.runningTotal).to.equal(18)
   })
 })
